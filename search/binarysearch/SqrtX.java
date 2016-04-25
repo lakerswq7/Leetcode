1e052f00b2a9e7fa09d7f71f3534df1c92ab5b8e
wq <wq19890727@gmail.com>
2016-04-24 23:13:01 -0700

    4.24

--- search/binarysearch/SqrtX.java
+++ search/binarysearch/SqrtX.java
@@ -1,32 +1,24 @@
 package search.binarysearch;
 
 public class SqrtX {
-    public int sqrt(int x) {
-        if (x < 0) {
-            return -1;
+    public int mySqrt(int x) {
+        if (x <= 0) {
+            return 0;
         }
-        int low = 0;
-        int high = x;
-        int mid;
-        long sqr;
+        int low = 1, high = x;
         while (low + 1 < high) {
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
             // 注意要在这里加long，如果直接定义mid为long，则在返回的时候会编译错误
             // 如果不加long，则右边计算完以后就直接溢出再赋给sqr，即使sqr是long也已经是错的值
-            sqr = (long)mid * mid;
+            long sqr = (long)mid * mid;
             if (sqr == x) {
                 return mid;
-            }
-            if (sqr > x) {
-                high = mid;
-            } else {
+            } else if (sqr < x) {
                 low = mid;
+            } else {
+                high = mid;
             }
         }
-        if (high * high == x) {
-            return high;
-        } else {
-            return low;
-        }
+        return low;
     }
 }
